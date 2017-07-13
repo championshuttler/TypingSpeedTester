2d3367de83c29f86b76242db385971755d7e1c90
championshuttler <shivams2799@gmail.com>
2017-07-14 01:27:03 +0530

    spell checking

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ var timer = [0,0,0,0];
 
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZero(time){
+  if (time<=9) {
+    time = "0"+ time;
+
+  }
+  return time;
+}
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
-  let currentTime = timer[0]+ ":" + timer[1] + ":" + timer[2];
+  let currentTime = leadingZero(timer[0])+ ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
   theTimer.innerHTML=currentTime;
   timer[3]++;
 
@@ -24,6 +31,19 @@ function runTimer(){
 // Match the text entered with the provided text on the page:
 function spellCheck(){
   let textEntered = testArea.value;
+  let originTextMatch=originText.substring(0,textEntered.length);
+  if (textEntered==originText) {
+    testWrapper.style.borderColor="#429890";
+
+  }else {
+    if (textEntered ==originTextMatch) {
+      testWrapper.style.borderColor="#65CCf3";
+
+    }
+    else {
+      testWrapper.style.borderColor= "#E95D0F";
+    }
+  }
   console.log(textEntered);
 }
 
